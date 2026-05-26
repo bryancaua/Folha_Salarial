206d99a9a0fb3d906cdbed3924c2fcb7a5f564d7
Bryan Cauã <bryanncaua@gmail.com>
2026-05-26 15:41:20 -0300

    Feat: ColaboradorService atualizado para se adaptar para cada tipo de colaborador guardado em nossa lista ao listalos

--- src/ColaboradorService.java
+++ src/ColaboradorService.java
@@ -10,15 +10,33 @@ public class ColaboradorService {
 
     public void listar () {
         for (Colaborador colaborador : colaboradores) {
-            System.out.println(
-                    "\n Registro do colaborador:" + colaborador.getRegistro() +
-                    "\n Nome do colaborador:" + colaborador.getNome() +
-                    "\n Tipo do colaborador:" + colaborador.getTipo() +
-                    "\n Quantidade de vendas do colaborador:" + colaborador.getVendas() +
-                    "\n Percentual de comissão do colaborador:" + colaborador.getPercentualComissao() +
-                    "\n Valor de peças vendidas:" + colaborador.getValorPorPeca() +
-                    "\n Quantidade de peças produzidas:" + colaborador.getQuantidadeProduzida()
-            );
+
+            if (colaborador.getTipo() == TipoColaborador.PADRAO) {
+                System.out.println(
+                                "\n Registro do colaborador:" + colaborador.getRegistro() +
+                                "\n Nome do colaborador:" + colaborador.getNome() +
+                                "\n Tipo do colaborador:" + colaborador.getTipo() +
+                                "\n Salário: " + colaborador.calcularSalarioFinal()
+                );
+            }  else if (colaborador.getTipo() == TipoColaborador.COMISSIONADO) {
+                        System.out.println(
+                            "\n Registro do colaborador:" + colaborador.getRegistro() +
+                                    "\n Nome do colaborador:" + colaborador.getNome() +
+                                    "\n Tipo do colaborador:" + colaborador.getTipo() +
+                                    "\n Quantidade de vendas do colaborador:" + colaborador.getVendas() +
+                                    "\n Percentual de comissão do colaborador:" + colaborador.getPercentualComissao() +
+                                    "\n Salário final: " + colaborador.calcularSalarioFinal()
+                        );
+            }  else if (colaborador.getTipo() == TipoColaborador.PRODUCAO) {
+                        System.out.println(
+                        "\n Registro do colaborador:" + colaborador.getRegistro() +
+                                "\n Nome do colaborador:" + colaborador.getNome() +
+                                "\n Tipo do colaborador:" + colaborador.getTipo() +
+                                "\n Valor de peças vendidas:" + colaborador.getValorPorPeca() +
+                                "\n Quantidade de peças produzidas:" + colaborador.getQuantidadeProduzida() +
+                                "\n Salário final: " + colaborador.calcularSalarioFinal()
+                        );
+            }
         }
     }
 }
