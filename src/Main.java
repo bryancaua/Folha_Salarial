9bc0f2b578a27b4594bc7aec2cb156f78b24c0e0
Bryan Cauã <bryanncaua@gmail.com>
2026-05-26 23:09:03 -0300

    Feat: Main.java, base para o ínicio do funciomaneto do projeto realizada.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  IO.println("Hello and welcome!");
-  System.out.println("Olá");
+// Criar um Switch com 2 opções se deseja cadastrar um colaborador ou listar eles // Colocar um verificador caso o usuário tenha escolhido outra opção sem ser 1 - 2 ou 0
 
-  for (int i = 1; i <= 5; i++) {
-    IO.println("i = " + i);
-  }
+import java.util.Scanner;
+
+void main() {
+    Scanner entrada = new Scanner(System.in);
+
+    int escolha = -1;
+
+    do {
+        System.out.println("=================================================");
+        System.out.println("=== Bem-vindo ao sistema de colaboradores! ===");
+        System.out.println("=================================================");
+        System.out.println("  1 - Cadastrar colaborador");
+        System.out.println("  2 - Listar colaboradores");
+        System.out.println("  0 - Sair");
+        System.out.println("=================================================");
+        System.out.print("Escolha uma opção: ");
+        escolha = entrada.nextInt();
+
+        switch (escolha) {
+            case 1:
+                //...
+                break;
+            case 2:
+                //...
+                break;
+            case 0:
+                //
+                System.out.println("O sistema será encerrado!");
+                break;
+
+            default:
+                System.out.println("Opção inválida!");
+        }
+    } while (escolha != 0);
+
+    entrada.close();
 }
